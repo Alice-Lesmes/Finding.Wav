b327258249ee04c7f2df24ea9efd66b07e22611a
Alice-Lesmes <alesmes3579@gmail.com>
2024-08-17 17:41:43 +1000

    Fixing more things yet again

--- app/src/main/java/com/example/findingwav/MainActivity.kt
+++ app/src/main/java/com/example/findingwav/MainActivity.kt
@@ -9,7 +9,6 @@ import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
 import android.provider.MediaStore.Audio
 import androidx.activity.compose.setContent
@@ -19,7 +18,15 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
@@ -35,7 +42,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
@@ -60,11 +72,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
-
-        var musicPlayer =
+        // Allows to play music when using changeSong()
+        var musicPlayer = MediaPlayer()
         setContent {
             FindingWavTheme {
-                // Inits the musicplayer. rn is loonboon
                 Scaffold(modifier =
 
                 Modifier.fillMaxSize()) { innerPadding ->
@@ -88,11 +99,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun changeSong(songPath : Uri) {
+    fun changeSong(songPath : Uri, mediaPlayer: MediaPlayer) {
       /**
       * This inits a music player and plays the song specified by the file path
       */
-        val mediaPlayer = MediaPlayer().apply {
+        mediaPlayer.apply {
             setAudioAttributes(
                 AudioAttributes.Builder()
                     .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
